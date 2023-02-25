5df7ee383eacdd557d36cffced50140d490a6681
RafayMemon01 <rafayrashid200611@gmail.com>
2023-02-25 19:43:39 +0500

    Completed

---
+++ java.js
@@ -0,0 +1,15 @@
+let UserValue = prompt("Enter Your Percentage Here")
+if (UserValue> 79) {
+    alert("Your Grade is A1")
+} else if (UserValue>69) {
+    alert("Your Grade is A")
+    
+}else if (UserValue>59){
+    alert("Your Grade is B")
+}else if(UserValue>49){
+    alert("Your Grade is C")
+}else if(UserValue>39){
+    alert("Your Result is D")
+}else{
+    alert("You are Fail")
+}
